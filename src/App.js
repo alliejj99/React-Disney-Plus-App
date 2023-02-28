c094c3253fd35b4c28238b6d0765a5a23fbc43c6
areum <alliejj99@gmail.com>
2023-02-28 21:12:47 +0900

    App component의 container 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import Nav from "./components/Nav";
 import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Nav />
-    </div>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.main`
+  position: relative;
+  top: 72px;
+  display: block;
+  min-height: calc(100vh - 250px);
+  padding: 0 calc(3.5vw + 5px);
+  overflow-x: hidden;
+
+  &:after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    opacity: 1;
+    z-index: -1;
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+  }
+`;
