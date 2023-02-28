b3533334325af143b9564a3a09c4b7d0833baa05
areum <alliejj99@gmail.com>
2023-02-28 17:59:34 +0900

    사용될 이미지와 비디오 다운 및 Nav Components 스타일

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [Get Movie API Key](https://www.themoviedb.org/)
 
-### Axios 인스턴스화 하는 이유
-
-중복된 부분을 계속 입력하지 않아도 되기 때문입니다.
+- React.js
+- axios
+- styled-components
--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Nav from "./components/Nav";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
