35a18e0b3ba01f1b1b1e62c608a8e48bd1b34c24
areum <alliejj99@gmail.com>
2023-02-28 21:31:04 +0900

    Banner Component 데이터 호출하기

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../api/axios";
+import requsts from "../api/request";
+import axiosRequest from "../api/request";
 import "./Banner.css";
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  const fetchData = async () => {
+    // 현재 상영 영화 정보 전체 불러오기
+    const request = await axiosInstance(axiosRequest.fetchNowPlaying);
+    // 현재 상영 영화 정보 특정 아이디 불러오기
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
+
+    // 특정 영화의 더 상세한 정보 가져오기
+    const { data: movieDetail } = await axiosInstance.get(`movie/${movieId}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    });
+
+    setMovie(movieDetail);
+
+    console.log(movie);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <div>Banner</div>;
 };
 
