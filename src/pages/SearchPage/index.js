5f19993655988984f99de2fd5842f8b1da56281d
areum <alliejj99@gmail.com>
2023-03-07 00:15:30 +0900

    검색 기능 완성

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,9 +1,11 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "../../api/axios";
+import "./SearchPage.css";
 
 const SearchPage = () => {
   const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
 
   const searchTerm = new URLSearchParams(useLocation().search);
 
@@ -12,8 +14,7 @@ const SearchPage = () => {
       const response = await axios.get(
         `/search/multi?include_adult=false&query=${queryData}`
       );
-      setSearchResults(response.data.result);
-      console.log("response", response);
+      setSearchResults(response.data.results);
     } catch (error) {
       console.log("error");
     }
@@ -25,7 +26,40 @@ const SearchPage = () => {
     }
   }, [fetchSearchMovies, searchTerm]);
 
-  return <div>searchpage</div>;
+  return (
+    <React.Fragment>
+      {searchResults.length > 0 ? (
+        <section className="search-container">
+          {searchResults.map((movie) => {
+            if (movie.backdrop_path !== null && movie.media_type !== "person") {
+              const movieImageUrl =
+                "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+              return (
+                <div className="movie" key={movie.id}>
+                  <div
+                    className="movie__column-poster"
+                    onClick={() => navigate(`/${movie.id}`)}
+                  >
+                    <img
+                      src={movieImageUrl}
+                      alt="movie"
+                      className="movie__poster"
+                    />
+                  </div>
+                </div>
+              );
+            }
+          })}
+        </section>
+      ) : (
+        <section className="no-results">
+          <div className="no-results-text">
+            <p>검색한 "{searchTerm && searchTerm}" 제목의 영화는 없습니다.</p>
+          </div>
+        </section>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default SearchPage;
